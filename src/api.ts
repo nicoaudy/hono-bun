4e194f721a16c4132517000934994e79ae8fa5de
Nico Audy <nicoaudy1994@gmail.com>
2024-01-29 08:57:04 +0700

    add cors, and logger middleware

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,16 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 import { userRouter, userNoAuthRouter } from '@/routes'
 
 const app = new Hono()
 
+app.use(cors())
+app.use(logger())
+
 app.route('/user', userNoAuthRouter)
 app.route('/user', userRouter)
 
-app.get('/', c => c.text('User API'))
+app.get('/', (c) => c.text('User API'))
 
 export default { port: Bun.env.APP_PORT, fetch: app.fetch }
